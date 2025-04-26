75d721dc63d9ec91c80695e877b3c59ecb6de8b1
lgomes25 <leomybi@gmail.com>
2025-04-26 15:19:02 -0300

    Criado modelos

---
+++ FolhaPagamento/src/model/Pessoa.java
@@ -0,0 +1,52 @@
+package model;
+
+import java.time.LocalDate;
+
+public abstract class Pessoa {
+
+	//Atributos
+	protected String nome;
+	protected String cpf;
+	protected LocalDate dtNasc;
+	
+	//Construtor
+	public Pessoa(String nome, String cpf, LocalDate dtNasc) {
+		this.nome = nome;
+		this.cpf = cpf;
+		this.dtNasc = dtNasc;
+	}
+
+	@Override
+	public String toString() {
+		return "Nome: " + nome + "\tCPF: " + cpf + "\tDt. Nasc.: " + dtNasc;
+	}
+
+	//Getters Setters
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public LocalDate getDtNasc() {
+		return dtNasc;
+	}
+
+	public void setDtNasc(LocalDate dtNasc) {
+		this.dtNasc = dtNasc;
+	}
+
+	
+	
+	
+}
