57da85d6b3d79bb99f4c454ceb33a86fd78a578a
lgomes25 <leomybi@gmail.com>
2025-04-26 22:32:43 -0300

    Adicionado atributo id e contagem de dependentes

--- FolhaPagamento/src/model/Funcionario.java
+++ FolhaPagamento/src/model/Funcionario.java
@@ -4,27 +4,30 @@ import java.time.LocalDate;
 
 public class Funcionario extends Pessoa {
 
-	//Atributos
+	// Atributos
+	private Integer id;
 	private Double salarioBruto;
 	private Double descontoInss;
 	private Double descontoIR;
-	
-	//Construtor
+	private int contDep;
+
+	// Construtor
 	public Funcionario(String nome, String cpf, LocalDate dtNasc, Double salarioBruto) {
 		super(nome, cpf, dtNasc);
 		this.salarioBruto = salarioBruto;
 		this.descontoInss = 0.;
 		this.descontoIR = 0.;
+		this.contDep = 0;
 	}
 
-	//toString
+	// toString
 	@Override
 	public String toString() {
-		return super.toString() + "\tSal. Bruto: " + salarioBruto + ",\tDescontoInss=" + descontoInss + ",\tDesc IR: "
-				+ descontoIR;
+		return super.toString() + "\tSal. Bruto: " + salarioBruto + ",\tDependentes: " + contDep + ",\tDescontoInss="
+				+ descontoInss + ",\tDesc IR: " + descontoIR;
 	}
 
-	//Getters Setters
+	// Getters Setters
 	public Double getSalarioBruto() {
 		return salarioBruto;
 	}
@@ -48,8 +51,21 @@ public class Funcionario extends Pessoa {
 	public void setDescontoIR(Double descontoIR) {
 		this.descontoIR = descontoIR;
 	}
-	
-	
-	
-	
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public int getContDep() {
+		return contDep;
+	}
+
+	public void setContDep(int contDep) {
+		this.contDep = contDep;
+	}
+
 }
