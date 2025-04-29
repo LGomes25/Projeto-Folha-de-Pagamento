e12b4980018be16be7f5aa7c9ad67f2f6f58c9de
lgomes25 <leomybi@gmail.com>
2025-04-29 07:43:18 -0300

    Criação classe Menu

---
+++ FolhaPagamento/src/util/Menu.java
@@ -0,0 +1,47 @@
+package util;
+
+public class Menu {
+
+	public static void principal() {
+		System.out.println("============= InteliFolha =============");
+		System.out.println("1 - Carregar arquivo externo (.csv)");
+		System.out.println("2 - Cadastrar novo Funcionário manualmente");
+		System.out.println("3 - Visualizar istas");
+		System.out.println("4 - Gerar arquivo (.csv)");
+		System.out.println("5 - Enviar listas para Banco de Dados");
+		System.out.println("6 - sair");
+		System.out.println("=======================================");
+		System.out.println("Escolha uma opção: ");
+	}
+	
+	//Carregar arquivo externo (.csv)
+	public static void sub1() {
+		System.out.println("Em Desenvolvimento...");
+	}
+	
+	//Cadastrar novo Funcionário manualmente
+	public static void sub2() {
+		System.out.println("Em Desenvolvimento...");
+	}
+	
+	//Visualizar istas
+	public static void sub3() {
+		System.out.println("Em Desenvolvimento...");
+	}
+	
+	//Gerar arquivo (.csv)
+	public static void sub4() {
+		System.out.println("Em Desenvolvimento...");
+	}
+	
+	//Enviar listas para Banco de Dados
+	public static void sub5() {
+		System.out.println("Em Desenvolvimento...");
+	}
+	
+	//sair
+	public static void sub6() {
+		System.out.println("Em Desenvolvimento...");
+	}
+	
+}
