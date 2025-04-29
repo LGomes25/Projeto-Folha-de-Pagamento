ace5fe596e50ab85e4ad3f51153e1f40cf73f1a0
lgomes25 <leomybi@gmail.com>
2025-04-29 08:08:11 -0300

    Criação de Classe DocsEstrutura

---
+++ FolhaPagamento/src/config/DocsEstrututra.java
@@ -0,0 +1,33 @@
+package config;
+
+public class DocsEstrututra {
+
+	/*
+	 * ├── app           # Implementação de menu com chamadas de funcionalidades
+	 * | ├── (RodaFolha) 
+	 * |
+	 * ├── config        # Configurações (Security, Beans, etc.)
+	 * | ├── (DocsEstrututra)
+	 * |
+	 * ├── service       # Lógica de negócios e serviços
+	 * |
+	 * ├── repository    # Acesso a banco de dados (JPA, JDBC, DAOs)
+	 * |
+	 * ├── model         # Entidades e DTOs
+	 * | ├──(Dependente)
+	 * | ├──(Funcionario)
+	 * | ├──(Pessoa)
+	 * |
+	 * ├── exception     # Tratamento de exceções
+	 * |
+	 * ├── config        # Configurações (Security, Beans, etc.)
+	 * |
+	 * ├── util          # Utilitários e classes auxiliares
+	 * | ├──(Menu)
+	 * | ├──(TipoDep)
+	 * | ├──(ValidaCpf)
+	 * | ├──(ValidaDependente)
+	 * 
+	 */
+	
+}
