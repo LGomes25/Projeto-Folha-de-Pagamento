f31b33ab36e5fbdafe37dbc9ee91b75a101aa9c3
lgomes25 <leomybi@gmail.com>
2025-04-30 11:26:30 -0300

    Formatação da estrutura em DocsEstrutura

--- FolhaPagamento/src/config/DocsEstrututra.java
+++ FolhaPagamento/src/config/DocsEstrututra.java
@@ -3,31 +3,38 @@ package config;
 public class DocsEstrututra {
 
 	/*
-	 * ├── app           # Implementação de menu com chamadas de funcionalidades
-	 * | ├── (RodaFolha) 
+	 * ├── app           		# Implementação de menu com chamadas de funcionalidades #
+	 * | ├── (RodaFolha)		=> Main class 
 	 * |
-	 * ├── config        # Configurações (Security, Beans, etc.)
-	 * | ├── (DocsEstrututra)
 	 * |
-	 * ├── service       # Lógica de negócios e serviços
-	 * | ├──(FolhaPagamento)
+	 * ├── config        		# Configurações (Security, Beans, etc.) 				#
+	 * | ├── (DocsEstrututra)	=> Armazena estrutura do projeto
+	 * |
+	 * |
+	 * ├── service       		# Lógica de negócios e serviços							#
+	 * | ├──(FolhaPagamento)	=> Calculos das folhas de pagamento
+	 * |
 	 * |
 	 * ├── repository    # Acesso a banco de dados (JPA, JDBC, DAOs)
 	 * |
-	 * ├── model         # Entidades e DTOs
-	 * | ├──(Dependente)
-	 * | ├──(Funcionario)
-	 * | ├──(Pessoa)
+	 * |
+	 * ├── model       			# Entidades e DTOs										#
+	 * | ├──(Dependente)		=> Objeto com informações sobre dependentes
+	 * | ├──(Funcionario)		=> Objeto com informações sobre Funcionarios
+	 * | ├──(Pessoa)			=> Objeto não instanciavel, SuperClass Abstract
+	 * |
 	 * |
 	 * ├── exception     # Tratamento de exceções
 	 * |
+	 * |
 	 * ├── config        # Configurações (Security, Beans, etc.)
 	 * |
-	 * ├── util          # Utilitários e classes auxiliares
-	 * | ├──(Menu)
-	 * | ├──(TipoDep)
-	 * | ├──(ValidaCpf)
-	 * | ├──(ValidaDependente)
+	 * |
+	 * ├── util          		# Utilitários e classes auxiliares						#
+	 * | ├──(Menu)				=> Métodos executáveis do menu (encapsulamento0
+	 * | ├──(TipoDep)			=> Enum de dependentes
+	 * | ├──(ValidaCpf)			=> Interface para validação de CPF
+	 * | ├──(ValidaDependente)	=> Interface para validação de idade de dependente
 	 * 
 	 */
 	
