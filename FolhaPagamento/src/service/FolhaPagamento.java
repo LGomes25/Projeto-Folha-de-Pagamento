083a4712d299cbbba5e3c1dc7de80e8e99f9e19e
lgomes25 <leomybi@gmail.com>
2025-05-01 18:32:58 -0300

    Add cálculos de INSS e IR

--- FolhaPagamento/src/service/FolhaPagamento.java
+++ FolhaPagamento/src/service/FolhaPagamento.java
@@ -1,7 +1,10 @@
 package service;
 
 import java.time.LocalDate;
+
 import model.Funcionario;
+import util.InssDescEnum;
+import util.IrDescEnum;
 
 public class FolhaPagamento {
 
@@ -20,9 +23,42 @@ public class FolhaPagamento {
 	//toString
 	@Override
 	public String toString() {
-		return  funcionario + "\tSal. Líquido: " + salLiquido +"\tDt. Pag: " + dataPagamento;
+		return  funcionario + String.format("\t-Sal. Líquido: %.2f\t-Dt. Pag: " + dataPagamento, salLiquido);
 	}
 
+	//Método para calcular desconto do INSS
+	public static Double calculaInss(Double salarioBruto) {
+		double calculoInss = 0.;
+		if (salarioBruto <= InssDescEnum.FAIXA1.getMaxSalFaixa()) {
+			calculoInss = ( salarioBruto * InssDescEnum.FAIXA1.getAliquotaInss())-InssDescEnum.FAIXA1.getDeduzirInss();
+		}else if (salarioBruto <= InssDescEnum.FAIXA2.getMaxSalFaixa()) {
+			calculoInss = ( salarioBruto * InssDescEnum.FAIXA2.getAliquotaInss())-InssDescEnum.FAIXA2.getDeduzirInss();
+		}else if (salarioBruto <= InssDescEnum.FAIXA3.getMaxSalFaixa()) {
+			calculoInss = ( salarioBruto * InssDescEnum.FAIXA3.getAliquotaInss())-InssDescEnum.FAIXA3.getDeduzirInss();
+		}else if (salarioBruto <= InssDescEnum.FAIXA4.getMaxSalFaixa()) {
+			calculoInss = ( salarioBruto * InssDescEnum.FAIXA4.getAliquotaInss())-InssDescEnum.FAIXA4.getDeduzirInss();
+		}else if (salarioBruto >= InssDescEnum.TETO.getMinSalFaixa()) {
+			calculoInss = InssDescEnum.DESCMAX.getDescMax();
+		}
+		return calculoInss;
+	}
+	
+	public static Double calculaIr(Double salarioBruto, Integer numDependente) {
+		Double calculoIr = 0.;
+		Double salBase = salarioBruto - (numDependente*IrDescEnum.DESCPORDEP.getDescontoPorDependente())- calculaInss(salarioBruto);
+		if (salBase <= IrDescEnum.FAIXA1.getSalMenorFaixa()) {
+			calculoIr = 0.;
+		} else if (salBase <= IrDescEnum.FAIXA2.getSalMaiorFaixa()) {
+			calculoIr = (salBase * IrDescEnum.FAIXA2.getAliquotaIr()) - IrDescEnum.FAIXA2.getDeduzirIr(); 
+		} else if (salBase <= IrDescEnum.FAIXA3.getSalMaiorFaixa()) {
+			calculoIr = (salBase * IrDescEnum.FAIXA3.getAliquotaIr()) - IrDescEnum.FAIXA3.getDeduzirIr(); 
+		} else if (salBase <= IrDescEnum.FAIXA4.getSalMaiorFaixa()) {
+			calculoIr = (salBase * IrDescEnum.FAIXA4.getAliquotaIr()) - IrDescEnum.FAIXA4.getDeduzirIr(); 
+		}else {
+			calculoIr = (salBase * IrDescEnum.FAIXA5.getAliquotaIr()) - IrDescEnum.FAIXA5.getDeduzirIr(); 
+		}
+		return calculoIr;
+	}
 	
 	
 	
