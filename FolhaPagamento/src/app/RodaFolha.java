3053c578256bb9b44359da65345d07ef48e2e603
lgomes25 <leomybi@gmail.com>
2025-05-03 22:02:10 -0300

    Add leitura e gravação .csv somente funcionarios

--- FolhaPagamento/src/app/RodaFolha.java
+++ FolhaPagamento/src/app/RodaFolha.java
@@ -1,6 +1,6 @@
 package app;
 
-import java.time.LocalDate;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -9,73 +9,36 @@ import model.Dependente;
 import model.Funcionario;
 import model.Pessoa;
 import service.FolhaPagamento;
-import util.TipoDep;
+
+//C:\serratec-2025-1\curso\dadosfuncionario.csv
 
 public class RodaFolha {
 
 	public static void main(String[] args) {
-
-		// Instanciamento de funcionarios
-		Funcionario fun1 = new Funcionario("Leonardo", "33344455567", LocalDate.of(2000, 2, 20), 1500.00);
-		Funcionario fun2 = new Funcionario("Mariana", "33344455569", LocalDate.of(1995, 5, 15), 2500.00);
-		Funcionario fun3 = new Funcionario("Carlos", "33344455569", LocalDate.of(1988, 10, 30), 3500.00);
-		Funcionario fun4 = new Funcionario("Ana", "44455566678", LocalDate.of(1992, 7, 8), 4500.00);
-		Funcionario fun5 = new Funcionario("Gabriel", "55566677781", LocalDate.of(2001, 12, 25), 6000.00);
-		Funcionario fun6 = new Funcionario("Jonas", "55566677780", LocalDate.of(2001, 11, 25), 10000.00);
-
-		// instanciamento dependentes
-		Dependente dep1 = new Dependente("José", "66655588812", LocalDate.of(2017, 02, 20), TipoDep.FILHO);
-		Dependente dep2 = new Dependente("Ana", "12345678912", LocalDate.of(2015, 11, 20), TipoDep.SOBRINHO);
-		Dependente dep3 = new Dependente("Lucas", "23456789123", LocalDate.of(2010, 3, 5), TipoDep.OUTROS);
-		Dependente dep4 = new Dependente("Beatriz", "34567891234", LocalDate.of(2017, 8, 15), TipoDep.FILHO);
-		Dependente dep5 = new Dependente("Gabriel", "45678912345", LocalDate.of(2014, 9, 25), TipoDep.SOBRINHO);
-		Dependente dep6 = new Dependente("João", "45678912344", LocalDate.of(2012, 5, 25), TipoDep.OUTROS);
+		
+		//Iniciar Scanner de leitura
+		Scanner sc = new Scanner(System.in);
 
 		// criando lista de dependente e funcionario
 		List<Funcionario> funcs = new ArrayList<>();
 		List<Dependente> deps = new ArrayList<>();
 		List<Pessoa> pessoas = new ArrayList<>();
 		List<FolhaPagamento> folhas = new ArrayList<>();
-
-		// Carregar listas funcionario, dependente e pessoa
-		funcs.add(fun1);
-		funcs.add(fun2);
-		funcs.add(fun3);
-		funcs.add(fun4);
-		funcs.add(fun5);
-		funcs.add(fun6);
-		deps.add(dep1);
-		deps.add(dep2);
-		deps.add(dep3);
-		deps.add(dep4);
-		deps.add(dep5);
-		deps.add(dep6);
-		pessoas.add(fun1);
-		pessoas.add(fun2);
-		pessoas.add(fun3);
-		pessoas.add(fun4);
-		pessoas.add(fun5);
-		pessoas.add(fun6);
-		pessoas.add(dep1);
-		pessoas.add(dep2);
-		pessoas.add(dep3);
-		pessoas.add(dep4);
-		pessoas.add(dep5);
-		pessoas.add(dep6);
-
 		
+		//Carregamento inicial automatico lista pre-prenchida via construtor
+		service.ListFuncTest.carregaLista(funcs, deps, pessoas);
 
-//		Apresenta dados da folha condensada 
-//		for (FolhaPagamento f : folhas) {
-//			System.out.println(f);
-//		}
+		//inicio da área de testes
+
+//		
+			
 
 //		\/verificar motivo pelo qual não funciona\/
 //		System.out.println(fun1.comparaCpf(fun1)); 
-//		 
+		 
+		//Fim da área de testes
+		
 //		Inicio de menu
-
-		Scanner sc = new Scanner(System.in);
 		int op = 0;// escolha para menus
 
 		while (op != 9) {
@@ -84,11 +47,12 @@ public class RodaFolha {
 			op = util.Menu.validaOp(sc); // chama leitura aceitando apenas numeros
 
 			switch (op) {
-			case 1:
-				System.out.println("Em desenvolvimento 1");
+			case 1: //importar arquivo .csv
+				FileReader arquivo = util.Menu.sub1(sc, funcs);//lê caminha do arquivo
+				service.ImportFileCsv.importArquivo(sc, arquivo, funcs);//importa dados*somente funcionarios*
 				break;
 
-			case 2:
+			case 2: //Cadastrar novo Funcionário manualmente
 				System.out.println("Em desenvolvimento 2");
 				break;
 
@@ -100,11 +64,11 @@ public class RodaFolha {
 				util.Menu.sub4(sc, funcs, deps, pessoas);
 				break;
 
-			case 5:
+			case 5: //Gerar arquivo (.csv)
 				System.out.println("Em desenvolvimento 5");
 				break;
 
-			case 6:
+			case 6: //Enviar listas para Banco de Dados
 				System.out.println("Em desenvolvimento 6");
 				break;
 
